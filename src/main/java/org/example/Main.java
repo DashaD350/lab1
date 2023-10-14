53d129382a880a57511b788b5054e127bba6f223
DashaD350 <dashaevora@gmail.com>
2023-10-14 06:15:45 +0300

    Крок 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,31 @@
 package org.example;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) { StringCalculator calculator = new StringCalculator();
+        Scanner strIng = new Scanner(System.in);
+        System.out.println("Enter your string: ");
+
+        String equation = strIng.nextLine();
+        System.out.println("Your string: " + equation);
+        System.out.println("Your result: ");
+        System.out.println(calculator.add(equation));
+    }
+}
+
+class StringCalculator {
+    public int add(String numbers){
+        if (numbers.isEmpty()) {
+            return 0;
+        }
+
+        String[] ourNumbers = numbers.split(",");
+        int sum = 0;
+
+        for (String num : ourNumbers) {
+            sum += Integer.parseInt(num);
+        }
+
+        return sum;
     }
 }
