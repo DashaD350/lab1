d80ca12241cc7c41ef898cbf9333d02357bc95e4
DashaD350 <dashaevora@gmail.com>
2023-10-14 06:21:53 +0300

    Крок 4

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) { StringCalculator calculator = new StringCalculator();
-        Scanner strIng = new Scanner(System.in);
+        Scanner str = new Scanner(System.in);
         System.out.println("Enter your string: ");
 
-        String equation = strIng.nextLine();
+        String equation = str.nextLine();
         System.out.println("Your string: " + equation);
         System.out.println("Your result: ");
         System.out.println(calculator.add(equation));
@@ -23,15 +23,31 @@ class StringCalculator {
             System.out.println("Not correct input");
         } else if (numbers.contains("\\n,")) {
             System.out.println("Not correct input");
-        }  else {
-            String[] ourNumbers = numbers.split("(,)|(\\\\n)");
-            int sum = 0;
+        } else {
+            if (numbers.startsWith("//")) {
+                int pastDelimiterIndex = numbers.indexOf("n");
+                String delimiter = numbers.substring(2, pastDelimiterIndex-1);
+                String numbersSubstring = numbers.substring(pastDelimiterIndex + 1);
 
-            for (String num : ourNumbers) {
-                sum += Integer.parseInt(num);
+                String[] ourNumbers = numbersSubstring.split(delimiter);
+
+                int sum = 0;
+                for (String num : ourNumbers) {
+                    sum += Integer.parseInt(num);
+                }
+
+                return sum;
+
+            } else
+            { String[] ourNumbers = numbers.split("(,)|(\\\\n)");
+                int sum = 0;
+
+                for (String num : ourNumbers) {
+                    sum += Integer.parseInt(num);
+                }
+
+                return sum;
             }
-
-            return sum;
         }
         return 0;
     }
