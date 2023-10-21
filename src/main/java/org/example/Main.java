0dc9e755e7c7b6540c8caacf0c721fb6732672c6
DashaD350 <dashaevora@gmail.com>
2023-10-21 09:05:19 +0300

    Крок 1 (виправлений)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,11 @@
 package org.example;
 import java.util.Scanner;
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) { StringCalculator calculator = new StringCalculator();
-        Scanner str = new Scanner(System.in);
+        Scanner strIng = new Scanner(System.in);
         System.out.println("Enter your string: ");
 
-        String equation = str.nextLine();
+        String equation = strIng.nextLine();
         System.out.println("Your string: " + equation);
         System.out.println("Your result: ");
         System.out.println(calculator.add(equation));
@@ -21,59 +18,16 @@ class StringCalculator {
             return 0;
         }
 
-        if (numbers.contains(",\\n")) {
-            System.out.println("Not correct input");
-        } else if (numbers.contains("\\n,")) {
-            System.out.println("Not correct input");
-        } else {
-            if (numbers.startsWith("//")) {
-                int pastDelimiterIndex = numbers.indexOf("n");
-                String delimiter = numbers.substring(2, pastDelimiterIndex-1);
-                String numbersSubstring = numbers.substring(pastDelimiterIndex + 1).replace(delimiter, ",");
+        String[] ourNumbers = numbers.split(",");
+        int sum = 0;
+        int size = ourNumbers.length;
 
-                String[] ourNumbers = numbersSubstring.split("(,)|(\\\\n)");
-
-                int sum = 0;
-
-                List <Integer> negative = new ArrayList<>();
-                for (String num : ourNumbers) {
-                    int value = Integer.parseInt(num);
-                    if (value < 0) {
-                        negative.add(value);
-                    }else if (value <= 1000){
-                        sum += value;
-                    }
-                }
-
-                if (!negative.isEmpty()) {
-                    System.out.println("Negative numbers not allowed: " + negative);
-                    return 0;
-                }
-
-                return sum;
-
-            } else
-            { String[] ourNumbers = numbers.split("(,)|(\\\\n)");
-                int sum = 0;
-
-                List <Integer> negative = new ArrayList<>();
-                for (String num : ourNumbers) {
-                    int value = Integer.parseInt(num);
-                    if (value < 0) {
-                        negative.add(value);
-                    } else if (value <= 1000){
-                        sum += value;
-                    }
-                }
-
-                if (!negative.isEmpty()) {
-                    System.out.println("Negative numbers not allowed: " + negative);
-                    return 0;
-                }
-
-                return sum;
+        if (size < 3){
+            for (String num : ourNumbers) {
+                sum += Integer.parseInt(num);
             }
         }
-        return 0;
+
+        return sum;
     }
 }
